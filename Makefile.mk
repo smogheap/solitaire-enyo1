bd705c69f35e8cbbaf413b02b4798d16a66bb9db
Micah N Gorrell <m@minego.net>
2014-10-20 10:36:55 -0600

    No longer depend on svn for version

--- Makefile.mk
+++ Makefile.mk
@@ -1,6 +1,6 @@
 APP			:= solitaire2
 VENDOR		:= net.minego
 APPID		:= $(VENDOR).$(APP)
-VERSION		:= $(shell (cd .. && svn info) | grep "Last Changed Rev:" | sed "s/.*: \([0-9]*\)\([0-9][0-9]\)/2.\1.\2/")
+VERSION		:= 2.21.09
 SRCS		:= $(shell find ../app -name "*.js" && find ../app -name "*.css" && find ../app -name "*.html")
 
